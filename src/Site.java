c631ff44704341ac5b1bdea58666df79bceb4db3
AmrAbdelazeem2 <tim@timrich.org>
2022-11-14 15:45:16 -0500

    Latest Update

--- src/Site.java
+++ src/Site.java
@@ -1,5 +1,20 @@
 public class Site {
     protected static final double TAX_RATE = 0;
+    private double BaseAmount;
+    private double TaxAmount;
     public Site() {
+    }
+
+    public double getBillableAmount() {
+
+        return getBaseAmount() + getTaxAmount();
+    }
+
+    public double getBaseAmount() {
+        return BaseAmount;
+    }
+
+    public double getTaxAmount() {
+        return TaxAmount;
     }
 }
